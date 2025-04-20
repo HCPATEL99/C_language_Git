ce735b04d543e75235c78434c0377d0bf8914fda
Harsiddh Patel <er.hcpatel123@gmail.com>
2025-04-20 14:28:21 +0530

    jnjsvv Updated
    Test commit 1
    
    Test Commit 2

--- src/firmware20.c
+++ src/firmware20.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
-#define FIRMWARE_VERSION "00.00.00.19"
+#define FIRMWARE_VERSION "00.00.00.20"
 int main()
 {
 	printf("Application Version - %s \n",FIRMWARE_VERSION);
-}
+
+
+#define "1st Commit"
+
+#define "2nd Commit"
